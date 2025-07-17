fa6de4f2bce972c9ebd49094a2eb0caafa894439
Rachel Tindoy <tindoyrachel@gmail.com>
2025-07-17 15:20:46 +0800

    Implement core app structure with basic styling

---
+++ index.php
@@ -0,0 +1,255 @@
+<!DOCTYPE html>
+<!--[if lt IE 10]>      <html class="no-js lt-ie11 lt-ie10 lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 10]>         <html class="no-js lt-ie11 lt-ie10 lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 11]>         <html class="no-js lt-ie11"> <![endif]-->
+<!--[if gt IE 11]><!--> <html class="no-js" lang="en-US"> <!--<![endif]-->
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!--[if IE]><meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"><![endif]-->
+
+    <title>COMPANYNAME</title>
+    <link rel="icon" href="images/favicon.png"/>
+    <link rel="stylesheet" href="css/hamburgers.min.css">
+    <link rel="stylesheet" href="css/animate.css">
+    <link rel="stylesheet" href="css/animate-4.1.1.css">
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="css/media.css">
+    
+  </head>
+  <body>
+
+<div class="header_holder">
+<header>
+    <div class="wrapper">
+      <div class="header_con">
+        <div class="main_logo">
+          <a href="index.php"><figure><img src="images/main-logo.png" alt="Logo"/></figure></a>
+        </div>
+
+        <div class="hamburger_con">
+          <div class="hamburger hamburger--stand">
+              <div class="hamburger-box">
+                <div class="hamburger-inner"></div>
+              </div>
+          </div>
+        </div>
+      </div>
+    </div>
+</header>
+
+<!-- Navigation -->
+<nav id="nav_area">
+    <ul class="nav_con">
+      <li class="current_page_item"><a href="">Home</a></li>
+      <li><a href="">About Us</a></li>
+      <li><a href="">Services</a></li>
+      <li><a href="">Careers</a></li>
+      <li><a href="">Contact Us</a></li>
+    </ul>
+</nav>
+<!-- End Navigation -->
+</div> <!-- header_holder -->
+
+
+<div id="banner">
+  <div class="w3-content w3-display-container">
+    <img class="mySlides" src="images/slider/1.jpg">
+    <img class="mySlides w3-animate-left" src="images/slider/2.jpg">
+    <img class="mySlides w3-animate-left" src="images/slider/3.jpg">
+
+   <!--  <button class="w3-button w3-display-left" onclick="plusDivs(-1)">&#10094;</button>
+    <button class="w3-button w3-display-right" onclick="plusDivs(+1)">&#10095;</button> -->
+  </div>
+
+
+  <!-- <div class="bnr_info">
+    <h2>This is a heading.</h2>
+    <p>Caramelization crema a pumpkin spice variety breve black iced arabica milk. Mug, crema, whipped chicory black, whipped trifecta a grounds body. Robust, carajillo, et cappuccino ut sugar aroma. Variety, acerbic as, steamed blue mountain rich barista skinny.</p>
+  </div> -->
+</div> <!-- #banner -->
+
+
+
+<div id="middle">
+  <div class="wrapper">
+    <div class="middle_con">
+
+      <div class="mid_info">
+    <!--?php dynamic_sidebar('mid_info');?-->
+        <h2>Great Coffee for Everyone!</h2>
+      <p>We serve coffee the way you like it. We offer specialty grade and directly traded coffee from organic and sustainable coffee farms.</p>
+    </div>
+
+
+    <div class="mid_boxes">
+      <section class="mid_box1">
+        <figure>
+          <!-- <img src="images/mid-box1.jpg" alt=""/> -->
+          <img src="images/mid-box1.jpg" alt="coffee latte"/>
+        </figure>
+
+        <div class="mid_box_info">
+          <!--?php dynamic_sidebar('mid_box1');?-->
+          <h2>Latte</h2>
+          <p>Italian origin made with espresso and steamed milk.</p>
+          <a href="#">Click Here</a>
+        </div>
+      </section>
+
+      <section class="mid_box2">
+        <figure>
+          <!-- <img src="images/mid-box2.jpg" alt=""/> -->
+          <img src="images/mid-box2.jpg" alt="capuccino coffee"/>
+        </figure>
+
+        <div class="mid_box_info">
+          <!--?php dynamic_sidebar('mid_box1');?-->
+          <h2>Capuccino</h2>
+          <p>Espresso shot and hot milk topped with foamed milk.</p>
+          <a href="#">Click Here</a>
+        </div>
+      </section>
+
+
+      <section class="mid_box3">
+        <figure>
+          <!-- <img src="images/mid-box3.jpg" alt=""/> -->
+          <img src="images/mid-box3.jpg" alt="espresso coffee"/>
+        </figure>
+
+        <div class="mid_box_info">
+          <!--?php dynamic_sidebar('mid_box1');?-->
+          <h2>Espresso</h2>
+          <p>Espresso is a coffee-brewing method of Italian origin.</p>
+          <a href="#">Click Here</a>
+        </div>
+      </section>
+
+      <section class="mid_box4">
+        <figure>
+          <!-- <img src="images/mid-box4.jpg" alt=""/> -->
+          <img src="images/mid-box4.jpg" alt="iced coffee"/>
+        </figure>
+
+        <div class="mid_box_info">
+          <!--?php dynamic_sidebar('mid_box1');?-->
+          <h2>Iced</h2>
+          <p>Regularly brewed coffee served over ice.</p>
+          <a href="#">Click Here</a>
+        </div>
+      </section>
+    </div>
+
+    </div>
+  </div>
+</div>
+<!-- End Middle -->
+
+
+<!-- Main -->
+<div id="main_area">
+  <div class="wrapper">
+    <div class="main_con">
+      <main>
+        <h1 class="h1_title"><span>Introducing </span>Coffee Enterprises Inc.</h1>
+
+         <p>Arabica Acerbic Affogato Aftertaste Aged Americano And Aroma id that blue spice, strong cinnamon filter grounds latte rich java Au whipped. Beans decaffeinated Aromatic plunger et extraction, lungo whipped latte eu instant espresso, mountain robusta con body. Roast medium cortado Brewed cup mountain carajillo Blue Aromatic, so turkish Aged Con Bar brewed flavour id, As to french siphon macchiato skinny filter.</p>
+      </main>
+      
+
+      <figure class="main_img">
+        <img src="images/main-img.jpg" alt="a person drinking coffee"/>
+      </figure>
+
+    </div>
+    <div class="clearfix"></div>
+  </div>
+</div>
+  <!-- End Main -->
+
+<!-- Bottom1 -->
+<div id="bottom1">
+<div class="wrapper">
+  <div class="btm1_main">
+
+     <div class="btm1_info wow bounceInUp">
+  <!--?php dynamic_sidebar('btm1_info');?-->
+     <h2>Birthday Bonus!</h2>
+        <p>Celebrate your birth month with Iced Coffee! Get Free Iced Coffee with minimum purchase of $10.</p>
+        <p>November 1 to December 31, 2021 only.</p>
+        <a href="#">More Info</a>
+  </div>
+
+  <figure class="btm1_img1">
+        <!-- <img src="images/btm1-box3.jpg" alt=""/> -->
+        <img src="images/btm1-img1.jpg" alt="a person holding two iced coffees"/>
+  </figure>
+
+
+
+  </div>
+</div>
+</div>
+
+
+<div id="bottom2">
+  <div class="wrapper">
+    <div class="btm2_main">
+
+    <div class="btm2_info">
+    <!--?php dynamic_sidebar('btm2_info');?-->
+       <h2>Write a Review</h2>
+      <p>Let us know how we can serve you better!</p>
+    </div>
+
+    <form method="post" action="*" class="btm2_form">
+      <input type="text" name="name" placeholder="Full Name" required>
+      <input type="email" name="email" placeholder="Email Address" required>
+      <textarea placeholder="Message"></textarea>
+      <input type="submit" name="submit" value="Send">
+    </form>
+
+    </div>
+  </div>
+</div>
+<!-- End Bottom -->
+
+
+<!-- Footer -->
+<footer>
+  <div id="footer_btm">
+    <div class="wrapper">
+      <div class="footer_btm_con">
+        <nav id="footer_nav">
+          <ul class="footer_nav_con">
+            <li class="current_page_item"><a href="">Home</a></li>
+            <li><a href="">About Us</a></li>
+            <li><a href="">Services</a></li>
+            <li><a href="">Careers</a></li>
+            <li><a href="">Contact Us</a></li>
+          </ul>
+        </nav>
+
+        <div class="copyright">
+        <p>
+            <span class="copyright_year">Copyright 2021</span>
+            <span class="separator">|</span>
+            <span class="designed">Designed by Rachel Tindoy</span>
+          </p>
+        </div>
+      </div> <!-- .footer_top_con -->
+    </div>
+  </div> <!-- #footer_top -->
+</footer>
+
+<script type="text/javascript" src="js/hamburgers.js"></script>
+<script type="text/javascript" src="js/jquery-3.6.0.min.js"></script>
+<script type="text/javascript" src="js/w3-css-slideshow.js"></script>
+<script type="text/javascript" src="js/wow.js"></script>
+<script type="text/javascript" src="js/plugins.js"></script>
+
+</body>
+</html>
+<!-- End Footer -->
