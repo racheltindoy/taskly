2ede59f26eef2bcb80f5805f7abdd113509a7bf1
Rachel Tindoy <tindoyrachel@gmail.com>
2025-07-17 22:42:24 +0800

    Add checkbox and update styling

--- js/javascript.js
+++ js/javascript.js
@@ -1,13 +1,19 @@
-const input = document.querySelector('input');
+const input = document.querySelector('#input_text');
 const addBtn = document.querySelector('#add');
 const list = document.querySelector('#list ul');
 
 
-
-
 addBtn.addEventListener('click', () => {
-	let item = document.createElement("li");
-	item.textContent = input.value;
-	list.appendChild(item);
+	let li = document.createElement('li');
+	let checkBox = document.createElement('input');
+	checkBox.type = 'checkbox';
+	checkBox.classList = 'checkBox';
+	let liText = document.createTextNode(input.value);
+
+	
+	li.appendChild(checkBox);
+	li.appendChild(liText);
+
+	list.appendChild(li);
 	input.value = '';
 });
