a839f51fb47c23b8cc179a16002bb11c559bbff4
Rachel Tindoy <tindoyrachel@gmail.com>
2025-07-18 17:16:58 +0800

    Add trash functionality

--- js/javascript.js
+++ js/javascript.js
@@ -18,7 +18,6 @@ let crossOut = () => {
     let listCheckboxes = document.querySelectorAll('.task-checkbox');
 
     listCheckboxes.forEach(listCheckbox => {
-
         listCheckbox.addEventListener('change', () => {
             if(listCheckbox.checked) {
                 listCheckbox.parentElement.style.textDecoration = 'line-through';
@@ -28,29 +27,57 @@ let crossOut = () => {
         })
 
     });
-}
+};
 
+
+let deleteList = () => {
+    let trashcans = document.querySelectorAll('.trash');
+
+    trashcans.forEach(trashcan => {
+        trashcan.addEventListener('click', () => {
+            trashcan.parentElement.remove();
+        })
+    });
+};
 
 addTaskButton.addEventListener('click', () => {
 	taskInput.focus();
 	if(taskInput.value == '') { return alert('Task is empty!'); }
 
+
+    // li
     const taskItem = document.createElement('li');
+
+    // li .listInfo
+    const taskItemListInfo = document.createElement('span');
+    taskItemListInfo.classList = 'listInfo';
+    taskItem.appendChild(taskItemListInfo);
+
+    // .listInfo checkbox
     const taskCheckbox = document.createElement('input');
     taskCheckbox.type = 'checkbox';
     taskCheckbox.classList = 'task-checkbox';
-    
-    const taskText = document.createTextNode(taskInput.value);
+    taskItemListInfo.appendChild(taskCheckbox);
 
-    taskItem.appendChild(taskCheckbox);
-    taskItem.appendChild(taskText);
+    // .listInfo text
+    const taskText = document.createTextNode(taskInput.value);
+    taskItemListInfo.appendChild(taskText);
+
+    // li .trash
+    const trash = document.createElement('span');
+    trash.classList = 'trash';
+    trash.textContent = '🗑️';
+    taskItem.appendChild(trash);
 
     taskList.appendChild(taskItem);
     taskInput.value = '';
+    
 
     setCheckboxArray();
     crossOut();
+    deleteList();
    
 });
 
- crossOut();
+crossOut();
+deleteList();
